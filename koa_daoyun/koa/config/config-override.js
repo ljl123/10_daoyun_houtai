52a97bea85c7be6a91cc888753c4ab2fad357333
李杰铃 <2228034998@qq.com>
2020-05-21 16:54:21 +0800

    Delete

--- koa_daoyun/koa/config/config-override.js
+++ koa_daoyun/koa/config/config-override.js
@@ -5,13 +5,13 @@ const SERVER_PORT_REMOTE = 16666
 
 var config = {
     dialect: 'mysql',
-    database: 'daoyun',
+    database: 'checkserver',
     username: 'root',
-    password: '123456',
+    password: '123',
     host: 'localhost',
     port: 3306,
     server_port: remote ? SERVER_PORT_REMOTE : SERVER_PORT_LOCAL,
-    imghost_default: "http://10.60.0.150:" + SERVER_PORT_LOCAL,
+    imghost_default: "http://192.168.1.101:" + SERVER_PORT_LOCAL,
     imghost: "localhost:" + SERVER_PORT_REMOTE,//服务器ip
     remote: remote,
     environment: { 1: log4js.levels.TRACE, 2: log4js.levels.DEBUG, 3: log4js.levels.INFO, 4: log4js.levels.WARN, 5: log4js.levels.ERROR },
