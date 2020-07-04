73fab8093a40e601e61caa05cb97e8102ec78218
jiangdongxiaobaiban <497409212@qq.com>
2020-07-04 17:47:22 +0800

    增加了字典字段

--- koa_daoyun/koa/models/DictInfoModel.js
+++ koa_daoyun/koa/models/DictInfoModel.js
@@ -3,6 +3,7 @@ const db = require('../db');
 module.exports = db.sequelize.define('dictinfo', {
     id: { type: db.type.INTEGER(8), primaryKey: true, allowNull: false, autoIncrement: true },
     typeid: { type: db.type.INTEGER(8), allowNull: false, comment: '类型id' },
+    mvalue: { type: db.type.INTEGER(8), allowNull: false, comment: '值' },
     info: { type: db.type.STRING(255), allowNull: false, comment: '文本描述' },
     typestate: { type: db.type.INTEGER(1), allowNull: false, defaultValue: 0, comment: '默认' }
 }, {
